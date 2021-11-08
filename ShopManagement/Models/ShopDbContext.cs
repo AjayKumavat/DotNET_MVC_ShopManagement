0146045f4cf9fc6c831f8716bc406eea86d78202
Ajay Kumawat <ajaykumavat@nimapinfotech.com>
2021-11-08 10:08:07 +0530

    Add project files.

---
+++ ShopManagement/Models/ShopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace ShopManagement.Models
+{
+    public class ShopDbContext : DbContext
+    {
+        public ShopDbContext() : base("con")
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
